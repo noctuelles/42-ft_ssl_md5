9869959e06dd821b0d41ee0abca8673052a5926f
noctuelles <plouvel@student.42.fr>
2024-07-02 15:07:05 +0200

    feat: command parsing

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -6,11 +6,21 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 14:52:53 by plouvel           #+#    #+#             */
-/*   Updated: 2024/07/02 14:52:58 by plouvel          ###   ########.fr       */
+/*   Updated: 2024/07/02 15:02:37 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_SSL_H
 #define FT_SSL_H
+
+#include <stdint.h>
+
+#include "ft_args_parser.h"
+
+typedef struct s_command {
+    const char           *name;
+    t_args_parser_config *opts_parsing_config;
+    const char *(*hash_fn)(const uint8_t *, size_t);
+} t_command;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,57 @@
 /*   By: plouvel <plouvel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 14:52:05 by plouvel           #+#    #+#             */
-/*   Updated: 2024/07/02 14:52:18 by plouvel          ###   ########.fr       */
+/*   Updated: 2024/07/02 15:06:12 by plouvel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
+#include "ft_ssl.h"
+#include "libft.h"
+
+#define NSIZE(x) (sizeof(x) / sizeof(x[0]))
+
+extern const char *program_invocation_short_name;
+
+static const t_command g_available_cmds[] = {{.name = "md5", .opts_parsing_config = NULL, .hash_fn = NULL},
+                                             {.name = "sha256", .opts_parsing_config = NULL, .hash_fn = NULL}};
+
+static void
+print_usage() {
+    printf("usage: %s [COMMAND] [COMMAND OPTS] \n\n", program_invocation_short_name);
+    printf("Available commands :\n");
+    for (size_t i = 0; i < NSIZE(g_available_cmds); i++) {
+        printf("%s\n", g_available_cmds[i].name);
+        if (g_available_cmds[i].opts_parsing_config == NULL) {
+            continue;
+        }
+        printf("Available options :\n");
+        ft_args_parser_print_docs(g_available_cmds[i].opts_parsing_config);
+    }
+}
+
 int
-main(void) {
+main(int argc, char **argv) {
+    const char      *cmd_str = NULL;
+    const t_command *cmd     = NULL;
+
+    if (argc < 2) {
+        print_usage();
+        return (1);
+    }
+    cmd_str = argv[1];
+    for (size_t i = 0; i < NSIZE(g_available_cmds); i++) {
+        if (strcmp(cmd_str, g_available_cmds[i].name) == 0) {
+            cmd = &g_available_cmds[i];
+            break;
+        }
+    }
+    if (cmd == NULL) {
+        ft_error(0, 0, "'%s': no such command\n", cmd_str);
+        print_usage();
+        return (1);
+    }
     return (0);
 }
